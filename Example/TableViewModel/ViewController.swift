db849ae139c25e911dff412efc6617deccdf18a5
tbergmen <tbergmen@gmail.com>
2015-11-20 19:18:20 +0200

    added a test cell to the sample app

--- Example/TableViewModel/ViewController.swift
+++ Example/TableViewModel/ViewController.swift
@@ -5,9 +5,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    var tableViewModel: TableViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableViewModel = TableViewModel(tableView: tableView)
+
+        let tableSection = TableSection(tableViewModel: tableViewModel)
+        tableViewModel.addSection(tableSection)
+
+        let row1 = TableRow(nibName: "TestCell1")
+        tableSection.addRow(row1)
     }
 
     override func didReceiveMemoryWarning() {
