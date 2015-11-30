26111db297b1e85bde9d2580d9bd82e4f76bdf76
tbergmen <tbergmen@gmail.com>
2015-11-30 14:50:28 +0200

    testing with multiple rows

--- Example/TableViewModel/ViewController.swift
+++ Example/TableViewModel/ViewController.swift
@@ -17,6 +17,18 @@ class ViewController: UIViewController {
 
         let row1 = TableRow(nibName: "TestCell1")
         tableSection.addRow(row1)
+
+        for var i = 0; i < 45; i++ {
+            let index = i
+            let row = TableRow(nibName: "TestCell2")
+            row.configureCell {
+                cell in
+                let testCell = cell as! TestCell2
+                testCell.button.setTitle("Button \(index)", forState: UIControlState.Normal)
+            }
+            row.height = 50
+            tableSection.addRow(row)
+        }
     }
 
     override func didReceiveMemoryWarning() {
